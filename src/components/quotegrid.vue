02108a119665118a17c1f7b51d6c255e782cdd64
JGolden25 <joechre@gmail.com>
2020-07-10 00:04:26 -0500

    fix:vbind added

--- src/components/quotegrid.vue
+++ src/components/quotegrid.vue
@@ -1,10 +1,16 @@
 <template>
     <div class="row">
+        <app-quote v-for="quote in quotes" v-bind:key="quote.id"> {{ quote }} </app-quote>
+    </div>
 </template>
 
 <script>
+    import Quote from './Quote.vue'
     export default {
-        props: ['quotes']
+        props: ['quotes'],
+        components: {
+            appQuote: Quote
+        }
     }
 </script>
 <style scoped>
