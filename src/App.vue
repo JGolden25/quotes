63f66f0e7181274b290e819c1e284c68c838a1f2
JGolden25 <joechre@gmail.com>
2020-07-10 11:57:48 -0500

    feat:adds alert for delete quotes

--- src/App.vue
+++ src/App.vue
@@ -26,6 +26,9 @@ import Header from './components/Header.vue';
         },
         methods: {
             newQuote(quote) {
+                if (this.quotes.length >= this.maxQuotes) {
+                    return alert('Please delete Quotes first!');
+                }
                 this.quotes.push(quote);
             },
             deleteQuote(index) {
